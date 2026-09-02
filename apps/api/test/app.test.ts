4301b7870f3ae9775cf804c73831e99ddd1868f3
joconnn <itsjustinoconnell@gmail.com>
2026-09-02 16:19:20 +0100

    vitest script and unknown route test

--- apps/api/test/app.test.ts
+++ apps/api/test/app.test.ts
@@ -1,20 +1,38 @@
 import { test, expect } from "vitest";
 import { buildApplication } from "../src/app";
 
-test("Http GET '/' route", async () => {
+// Our test case
+test("GET request to '/' route", async () => {
+  // create our fastify app
   const app = buildApplication();
 
   try {
+    // Use fastify inject to mock the http server
     const response = await app.inject({
       method: "GET",
       url: "/",
     });
-
+    // Access reponse values
     expect(response.statusCode).toBe(200);
     expect(response.json()).toEqual({
       hello: "world",
     });
   } finally {
+    // close down fastify server
     await app.close();
+  }
+});
+
+test("GET request to an unknown route", async () => {
+  const app = buildApplication();
+
+  try {
+    const response = await app.inject({
+      method: "GET",
+      url: "/unknown-route",
+    });
+    expect(response.statusCode).toBe(404);
+  } finally {
+    app.close();
   }
 });
